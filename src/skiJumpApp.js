1b976013a7492250bbaef185e240c583d332ad38
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-10-31 11:48:25 +0100

    Create canvas and background class

--- src/skiJumpApp.js
+++ src/skiJumpApp.js
@@ -6,7 +6,7 @@ const game = {
   desciption: 'Sky jump game with lots of snow',
   canvasDOM: undefined,
   ctx: undefined,
-  canvasSize: { width: undefined, height: undefined },
+  canvasSize: { width: 500, height: 300 },
   framesCounter: 0,
   frames: 60,
   intervalId: undefined,
@@ -16,10 +16,55 @@ const game = {
   obstacles: [],
   keys: {
     player: {
-    // TODO:
-    //   SPACE: " ",
-    //   ARROW_UP: "ArrowUp"
+      ARROW_UP: "ArrowUp",
+      ARROW_DOWN: "ArrowDown"
     }
+  },
+
+  init() {
+    this.setContext()
+    this.setDimensions()
+    this.createAll()
+    this.start()
+  },
+
+  setContext() {
+    this.canvasDOM = document.querySelector(".canvas")
+    this.ctx = this.canvasDOM.getContext("2d")
+  },
+
+  setDimensions() {
+
+    this.canvasDOM.setAttribute("width", this.canvasSize.width)
+    this.canvasDOM.setAttribute("height", this.canvasSize.height)
+  },
+
+  start(){
+    this.drawBackground()
+  },
+
+
+  drawBackground() {
+    console.log("pintando Background")
+    // this.ctx.fillRect(0, 0, this.canvasSize.width, this.canvasSize.height)
+    this.background.draw()
+  },
+  
+  createBackground() {
+    console.log("creando Background")
+    this.background = new Background(this.ctx, 0, 0, this.canvasSize.width, this.canvasSize.height, 5, "bg.png")
+  },
+
+  moveBackground() {
+    console.log("moviendo Background")
+    this.background.move()
+  },
+
+
+  createAll() {
+    this.createBackground()
+ /*    this.createPlayer()
+    this.createScoreBoard() */
   },
 
   // TODO: Métodos init(), setContext(), setDimensions(), start(), createAll()
