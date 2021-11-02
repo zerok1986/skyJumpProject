e34e52d120a6cf2dd08c60a8355c06928d502f1d
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-02 14:06:30 +0100

    Complete powerUps sprite animation and collision animation

--- src/PowerUp.js
+++ src/PowerUp.js
@@ -1,37 +1,103 @@
 class PowerUp {
-	constructor(ctx, posX, posY, width, height, slope, speedY) {
-		this.ctx = ctx
+  constructor(
+    ctx,
+    posX,
+    posY,
+    width,
+    height,
+    slope,
+    speedY,
+    imageName,
+    collisionImageName
+  ) {
+    this.ctx = ctx;
 
-		this.pos = {
-			x: posX,
-			y: posY,
-			radius: width / 2
-		}
+    this.pos = {
+      x: posX,
+      y: posY,
+      radius: width / 2,
+    };
 
-		this.size = {
-			width: width,
-			height: height
-		}
+    this.size = {
+      width: width,
+      height: height,
+    };
 
-		this.speed = {
-			y: speedY
-		}
+    this.speed = {
+      y: speedY,
+    };
 
-		this.slope = {
-			angle: slope.angle,
-			start: { x: slope.start.x, y: slope.start.y },
-			end: { x: slope.end.x, y: slope.end.y },
-		};
+    this.imageInstance = undefined;
+    this.imageName = imageName;
 
-	}
+    this.collisionImageInstance = undefined;
+    this.collisionImageName = collisionImageName;
 
-    draw() {
-        this.ctx.fillStyle = '#15FD37'
-        this.ctx.fillRect(this.pos.x, this.pos.y, this.size.width, this.size.height)
+    this.isCollision = 0;
+
+    this.frameIndex = 0;
+
+    this.spriteSource = {
+      source: { x: 0, y: 0 },
+      size: { width: 190, height: 190 },
+    };
+
+    this.slope = {
+      angle: slope.angle,
+      start: { x: slope.start.x, y: slope.start.y },
+      end: { x: slope.end.x, y: slope.end.y },
+    };
+
+    this.init();
+  }
+
+  init() {
+    this.imageInstance = new Image();
+    this.imageInstance.src = `../img/${this.imageName}`;
+
+    this.collisionImageInstance = new Image();
+    this.collisionImageInstance.src = `../img/${this.collisionImageName}`;
+  }
+
+  draw() {
+    if (this.isCollision !== 1) {
+      this.ctx.drawImage(
+        this.imageInstance,
+        this.pos.x,
+        this.pos.y,
+        this.size.width,
+        this.size.height
+      );
     }
+  }
 
-    move() {
-        this.pos.x -= this.speed.y
+  drawCollision() {
+    this.ctx.drawImage(
+      this.collisionImageInstance,
+      this.spriteSource.source.x +
+        this.spriteSource.size.width * this.frameIndex,
+      this.spriteSource.source.y,
+      this.spriteSource.size.width,
+      this.spriteSource.size.height,
+      this.pos.x,
+      this.pos.y,
+      this.size.width,
+      this.size.height
+    );
+    console.log(this.spriteSource.size.width * this.frameIndex);
+    if (game.framesCounter % 2 === 0) {
+      this.animate();
     }
+  }
 
+  animate() {
+    if (this.frameIndex === 9) {
+      this.spriteSource.source.x = 100;
+    }
+    this.frameIndex++;
+  }
+
+  move() {
+    this.pos.x -= this.speed.y;
+  }
 }
--- src/skiJumpApp.js
+++ src/skiJumpApp.js
@@ -1,9 +1,9 @@
 const game = {
-  title: 'Ski Jump App',
-  author: 'Mauro Monereo & Miguel Angel Abad',
+  title: "Ski Jump App",
+  author: "Mauro Monereo & Miguel Angel Abad",
   license: undefined,
-  version: '0.0.1',
-  desciption: 'Sky jump game with lots of snow',
+  version: "0.0.1",
+  desciption: "Sky jump game with lots of snow",
   canvasDOM: undefined,
   ctx: undefined,
   canvasSize: { width: 1000, height: 600 },
@@ -23,161 +23,164 @@ const game = {
   slopeInstance: undefined,
   slope: {
     angle: 20,
-    start: {x: 0, y: 42},   ///// x is always 0 y is 7% of canvas height
-    end: {x:1300, y: 360}	//// x is 130% of canvas width y is 60% of canvas height
+    start: { x: 0, y: 42 }, ///// x is always 0 y is 7% of canvas height
+    end: { x: 1300, y: 360 }, //// x is 130% of canvas width y is 60% of canvas height
   },
   moving: 0,
   keys: {
     player: {
       ARROW_UP: "ArrowUp",
-      ARROW_DOWN: "ArrowDown"
-    }
+      ARROW_DOWN: "ArrowDown",
+    },
   },
 
   init() {
-    this.setContext()
-    this.setDimensions()
-    this.createAll()
-    this.setListeners()
-    
-    this.start()
+    this.setContext();
+    this.setDimensions();
+    this.createAll();
+    this.setListeners();
+
+    this.start();
   },
 
   setContext() {
-    this.canvasDOM = document.querySelector(".canvas")
-    this.ctx = this.canvasDOM.getContext("2d")
+    this.canvasDOM = document.querySelector(".canvas");
+    this.ctx = this.canvasDOM.getContext("2d");
   },
 
   setDimensions() {
-    this.canvasDOM.setAttribute("width", this.canvasSize.width)
-    this.canvasDOM.setAttribute("height", this.canvasSize.height)
+    this.canvasDOM.setAttribute("width", this.canvasSize.width);
+    this.canvasDOM.setAttribute("height", this.canvasSize.height);
   },
 
-  start(){
+  start() {
     this.intervalId = setInterval(() => {
-      this.framesCounter++
+      this.framesCounter++;
       if (this.framesCounter > 2000) {
-        this.framesCounter = 0
+        this.framesCounter = 0;
       }
-      this.setObstacleFrequency()
+      this.setObstacleFrequency();
       if (this.framesCounter % 300 === 0) {
-        this.createPowerUp()
+        this.createPowerUp();
       }
       this.clearScreen();
-      this.drawAll()
-      this.moveAll()
-      this.clearObstacles()
-      this.collisionResult(this.isCollision())
-      this.isCollisionPowerUp()
-      this.clearPowerUps()
-      this.updateSpeed()
-      console.log(this.isCollisionPowerUp())
-      console.log(this.obstaclesSpeed)
-  /*    console.log("obstacles: ", this.obstacles)
-      console.log('this obstacles speed:' ,this.obstaclesSpeed) */
-    
-    }, 1000 / this.FPS)
-   
+      this.drawAll();
+      this.moveAll();
+      this.clearObstacles();
+      this.collisionResult(this.isCollision());
+      this.isCollisionPowerUp();
+      this.clearPowerUps();
+      this.updateSpeed();
+    }, 1000 / this.FPS);
   },
 
-  setObstacleFrequency(){
-    if (this.obstaclesSpeed > 10){
-          
+  setObstacleFrequency() {
+    if (this.obstaclesSpeed > 10) {
       if (this.framesCounter % 50 === 0) {
-            this.createObstacle();
-          }
+        this.createObstacle();
+      }
+    } else {
+      if (this.framesCounter % 100 === 0) {
+        this.createObstacle();
+      }
     }
-    else{
-          if (this.framesCounter % 100 === 0) {
-            this.createObstacle();
-          }
-    }
-
   },
 
   reset() {
-    this.clearScreen()
-    this.framesCounter = 0
-    this.ctx = undefined
-    this.canvasDOM = undefined
-    this.intervalId = undefined
-    this.backgroundSpeed = 1
-    this.obstacles = []
-    this.powerUps = []
-    this.score = 0
-    this.player = undefined
-    this.isCollisionCount = 0
-    this.isCollisionDodgedCount = 0
-    this.obstaclesSpeed = 1
-    this.init()
+    this.clearScreen();
+    this.framesCounter = 0;
+    this.ctx = undefined;
+    this.canvasDOM = undefined;
+    this.intervalId = undefined;
+    this.backgroundSpeed = 1;
+    this.obstacles = [];
+    this.powerUps = [];
+    this.score = 0;
+    this.player = undefined;
+    this.isCollisionCount = 0;
+    this.isCollisionDodgedCount = 0;
+    this.obstaclesSpeed = 1;
+    this.init();
   },
-  
-  resetContext(){
+
+  resetContext() {
     this.ctx.rotate((this.slope.angle * -1 * Math.PI) / 180);
   },
 
-  drawAll(){
-    this.drawBackground()
-    this.updateScore()
-    this.updateLifes()
-    this.drawSlope()
-    this.drawPlayer()
-    this.drawObstacles()
-    this.drawPowerUps()
-    this.resetContext()
+  drawAll() {
+    this.drawBackground();
+    this.updateScore();
+    this.updateLifes();
+    this.drawSlope();
+    this.drawPlayer();
+    this.drawObstacles();
+    this.drawPowerUps();
+    this.resetContext();
   },
 
   drawBackground() {
     // this.ctx.fillRect(0, 0, this.canvasSize.width, this.canvasSize.height)
-    this.background.draw()
+    this.background.draw();
     // console.log("pintando Background")
   },
 
-  drawSlope(){
-    this.ctx.rotate(this.slope.angle * Math.PI / 180);
-    this.slopeInstance.draw()
+  drawSlope() {
+    this.ctx.rotate((this.slope.angle * Math.PI) / 180);
+    this.slopeInstance.draw();
     // console.log("pintando slope")
   },
 
-  drawPlayer(){
-    this.player.draw(0,0,60,50)
-    
+  drawPlayer() {
+    this.player.draw(0, 0, 60, 50);
   },
 
   drawObstacles() {
-    this.obstacles.forEach(obs => obs.draw())
+    this.obstacles.forEach((obs) => obs.draw());
   },
 
   drawPowerUps() {
-    this.powerUps.forEach(pwu => pwu.draw())
+    this.powerUps.forEach((pwu) => {
+      if (pwu.isCollision === 1) {
+        pwu.drawCollision();
+      }
+      pwu.draw();
+    });
   },
 
   calculateScore() {
-    this.score += Math.floor(this.obstaclesSpeed * 0.4)
+    this.score += Math.floor(this.obstaclesSpeed * 0.4);
   },
 
-  updateScore(){
-    this.calculateScore()
-    scoreHTML.innerHTML = `Score: ${this.score}`
+  updateScore() {
+    this.calculateScore();
+    scoreHTML.innerHTML = `Score: ${this.score}`;
   },
 
-  updateLifes(){
-    lifesHTML.innerHTML = `${this.lifes}`
+  updateLifes() {
+    lifesHTML.innerHTML = `${this.lifes}`;
   },
 
-  moveAll(){
-    this.moveBackground()
-    this.moveSlope()
-    this.moveObstacle()
-    this.movePowerUps()
+  moveAll() {
+    this.moveBackground();
+    this.moveSlope();
+    this.moveObstacle();
+    this.movePowerUps();
   },
 
   createBackground() {
-    this.background = new Background(this.ctx, 0, 0, this.canvasSize.width, this.canvasSize.height, this.backgroundSpeed, "bm-view2.png")
+    this.background = new Background(
+      this.ctx,
+      0,
+      0,
+      this.canvasSize.width,
+      this.canvasSize.height,
+      this.backgroundSpeed,
+      "bm-view2.png"
+    );
     //console.log("creando Background")
   },
 
-  createSlope(){
+  createSlope() {
     this.slopeInstance = new Slope(
       this.ctx,
       this.slope.start.x,
@@ -190,19 +193,27 @@ const game = {
   },
 
   createPlayer() {
+    // player starting x is 20% of canvas x
+    let startingX = this.canvasSize.width * 0.2;
+    // player starting y is 33% of canvas y
+    let startingY = this.canvasSize.height * 0.33;
+    // playeer ending x and y is 15% of player starting x and y respectively
+    let width = startingX * 0.25;
+    let height = startingY * 0.25;
 
-	// player starting x is 20% of canvas x
-	let startingX = this.canvasSize.width * 0.20;
-	// player starting y is 33% of canvas y
-	let startingY = this.canvasSize.height * 0.33;
-	// playeer ending x and y is 15% of player starting x and y respectively
-	let width = startingX * 0.25;
-	let height = startingY * 0.25;
-
-    this.player = new Player(this.ctx, startingX, startingY, width, height, this.slope, 7, 'player-sprite.png')
+    this.player = new Player(
+      this.ctx,
+      startingX,
+      startingY,
+      width,
+      height,
+      this.slope,
+      7,
+      "player-sprite.png"
+    );
     //console.log("creando Player")
   },
-  
+
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
@@ -212,11 +223,22 @@ const game = {
       this.slope.start.y,
       this.slope.end.y - 20
     );
- /*    obstacle width is 2.30% of slope x
+    /*    obstacle width is 2.30% of slope x
 	let width = (this.slope.end.x - this.slope.start.x) * 0.023
     obstacle height is 9.40% of slope y
 	let height = (this.slope.end.y - this.slope.start.y) * 0.094 */
-    this.obstacles.push(new Obstacle(this.ctx, this.slope.end.x, randomY, 25, 25, this.slope, this.obstaclesSpeed, 'flag-sprite.png'))
+    this.obstacles.push(
+      new Obstacle(
+        this.ctx,
+        this.slope.end.x,
+        randomY,
+        25,
+        25,
+        this.slope,
+        this.obstaclesSpeed,
+        "flag-sprite.png"
+      )
+    );
   },
 
   createPowerUp() {
@@ -224,181 +246,190 @@ const game = {
       this.slope.start.y,
       this.slope.end.y - 20
     );
-    this.powerUps.push(new PowerUp(this.ctx, this.slope.end.x, randomY, 10, 10, this.slope, 10))
+    this.powerUps.push(
+      new PowerUp(
+        this.ctx,
+        this.slope.end.x,
+        randomY,
+        40,
+        40,
+        this.slope,
+        7,
+        "powerUp-sprite.png",
+        "powerUp-explosion.png"
+      )
+    );
   },
 
   moveBackground() {
-    this.background.move()
+    this.background.move();
     //console.log("moviendo Background")
   },
 
-  moveSlope(){
-    this.slopeInstance.move()
+  moveSlope() {
+    this.slopeInstance.move();
   },
 
   moveObstacle() {
-    this.obstacles.forEach(obs => obs.move())
+    this.obstacles.forEach((obs) => obs.move());
   },
 
   movePowerUps() {
-    this.powerUps.forEach(pwu => pwu.move())
+    this.powerUps.forEach((pwu) => pwu.move());
   },
 
-
   clearScreen() {
-    this.ctx.clearRect(0, 0, this.canvasSize.width, this.canvasSize.height)
+    this.ctx.clearRect(0, 0, this.canvasSize.width, this.canvasSize.height);
   },
 
   createAll() {
-    this.createBackground()
-    this.createSlope()
-    this.createPlayer()
+    this.createBackground();
+    this.createSlope();
+    this.createPlayer();
   },
 
   setListeners() {
     document.onkeydown = (e) => {
       if (e.key === this.keys.player.ARROW_UP) {
-		this.moving = 1;
-        this.player.spriteSource.source.x = 185
+        this.moving = 1;
+        this.player.spriteSource.source.x = 185;
         this.player.spriteSource.source.y = 0;
-        this.player.moveUp()
+        this.player.moveUp();
       }
       if (e.key === this.keys.player.ARROW_DOWN) {
-		this.moving = 1;
+        this.moving = 1;
         this.player.spriteSource.source.x = 120;
         this.player.spriteSource.source.y = 0;
-        this.player.moveDown()
+        this.player.moveDown();
       }
-    }
+    };
 
     document.onkeyup = (e) => {
       if (e.key === this.keys.player.ARROW_UP) {
-		this.moving = 0;
-        this.player.spriteSource.source.x = 0
-        this.player.spriteSource.source.y = 0
+        this.moving = 0;
+        this.player.spriteSource.source.x = 0;
+        this.player.spriteSource.source.y = 0;
       }
       if (e.key === this.keys.player.ARROW_DOWN) {
-		this.moving = 0;
-        this.player.spriteSource.source.x = 0
-        this.player.spriteSource.source.y = 0
+        this.moving = 0;
+        this.player.spriteSource.source.x = 0;
+        this.player.spriteSource.source.y = 0;
       }
-    }
+    };
   },
 
   clearObstacles() {
-    this.obstacles = this.obstacles.filter(obs => {
+    this.obstacles = this.obstacles.filter((obs) => {
       if (obs.pos.x > 0) {
-        return true
+        return true;
       }
-    })
+    });
   },
 
   clearPowerUps() {
-    this.powerUps = this.powerUps.filter(pwu => {
+    this.powerUps = this.powerUps.filter((pwu) => {
       if (pwu.pos.x > 0) {
-        return true
+        return true;
       }
-    })
+    });
   },
 
   isCollision() {
-      return this.obstacles.some((obs) => {
-      let dx = (this.player.pos.x + this.player.pos.radius) - (obs.pos.x + obs.pos.radius);
-      let dy = (this.player.pos.y + this.player.pos.radius) - (obs.pos.y + obs.pos.radius);
+    return this.obstacles.some((obs) => {
+      let dx =
+        this.player.pos.x +
+        this.player.pos.radius -
+        (obs.pos.x + obs.pos.radius);
+      let dy =
+        this.player.pos.y +
+        this.player.pos.radius -
+        (obs.pos.y + obs.pos.radius);
       let distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < this.player.pos.radius + obs.pos.radius) {
-          this.obstaclesSpeed = 1
-          this.lifes -= 10
-          obs.spriteSource.source.x = 42
+        this.obstaclesSpeed = 1;
+        this.lifes -= 10;
+        obs.spriteSource.source.x = 42;
       }
-    })
+    });
   },
 
   isCollisionPowerUp() {
-      return this.powerUps.some((pws) => {
-      let dx = (this.player.pos.x + this.player.pos.radius) - (pws.pos.x + pws.pos.radius);
-      let dy = (this.player.pos.y + this.player.pos.radius) - (pws.pos.y + pws.pos.radius);
+    return this.powerUps.some((pws) => {
+      let dx =
+        this.player.pos.x +
+        this.player.pos.radius -
+        (pws.pos.x + pws.pos.radius);
+      let dy =
+        this.player.pos.y +
+        this.player.pos.radius -
+        (pws.pos.y + pws.pos.radius);
       let distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < this.player.pos.radius + pws.pos.radius) {
-          this.lifes+= 5;
-          console.log("Collision POWER-UP")
+        pws.isCollision = 1;
+        this.lifes++;
       }
-    })
+    });
   },
-
 
   collisionResult(result) {
-    result === true ? this.isCollisionCount++ : this.isCollisionDodgedCount++
+    result === true ? this.isCollisionCount++ : this.isCollisionDodgedCount++;
   },
 
-  updateSpeed(){
-    this.updateObstacleSpeed()
-    this.updateBackgroundSpeed()
-    this.updatePlayerSpeed()
+  updateSpeed() {
+    this.updateObstacleSpeed();
+    this.updateBackgroundSpeed();
+    this.updatePlayerSpeed();
   },
 
   updateObstacleSpeed() {
- 
     if (this.obstaclesSpeed > 0 && this.obstaclesSpeed < 5) {
-      if (this.isCollisionDodgedCount > 300){
-        this.obstaclesSpeed += 1
-        this.isCollisionDodgedCount = 0
+      if (this.isCollisionDodgedCount > 300) {
+        this.obstaclesSpeed += 1;
+        this.isCollisionDodgedCount = 0;
       }
-    } 
-    else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
+    } else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
       if (this.isCollisionDodgedCount > 200) {
-          this.obstaclesSpeed += 2
-          this.isCollisionDodgedCount = 0
-        }
-    }
-    else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
+        this.obstaclesSpeed += 2;
+        this.isCollisionDodgedCount = 0;
+      }
+    } else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
       if (this.isCollisionDodgedCount > 100) {
-          this.obstaclesSpeed += 3
-          this.isCollisionDodgedCount = 0
-        }
+        this.obstaclesSpeed += 3;
+        this.isCollisionDodgedCount = 0;
+      }
     }
   },
 
-  updateBackgroundSpeed(){
+  updateBackgroundSpeed() {
     if (this.obstaclesSpeed > 0 && this.obstaclesSpeed < 3) {
-      this.background.speed.x = 2
-      this.slopeInstance.speed.x = 2
-    }
-    else if (this.obstaclesSpeed >= 3 && this.obstaclesSpeed < 5) {
+      this.background.speed.x = 2;
+      this.slopeInstance.speed.x = 2;
+    } else if (this.obstaclesSpeed >= 3 && this.obstaclesSpeed < 5) {
       this.background.speed.x = 4;
       this.slopeInstance.speed.x = 4;
-    }
-    else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
+    } else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
       this.background.speed.x = 6;
-      this.slopeInstance.speed.x = 6
-    }
-    else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
+      this.slopeInstance.speed.x = 6;
+    } else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
       this.background.speed.x = 8;
-      this.slopeInstance.speed.x = 8
+      this.slopeInstance.speed.x = 8;
     }
-    
   },
 
   updatePlayerSpeed() {
     if (this.obstaclesSpeed > 0 && this.obstaclesSpeed < 5) {
-      this.player.speed.y = 7
-    }
-    else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
+      this.player.speed.y = 7;
+    } else if (this.obstaclesSpeed >= 5 && this.obstaclesSpeed < 10) {
       this.player.speed.y = 10;
-    }
-    else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
+    } else if (this.obstaclesSpeed >= 10 && this.obstaclesSpeed < 15) {
       this.player.speed.y = 15;
     }
-   
-  }
-
-
-    
+  },
 
   /* TODO:  
             Sprites PowerUps (animar a colisión)
             Retocar el asset del Background
             */
-}
+};
