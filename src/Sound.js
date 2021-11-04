72f8d6d9696a773943d93ba02e7292f59c73ce14
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-04 13:46:20 +0100

    changed sound path

--- src/Sound.js
+++ src/Sound.js
@@ -1,8 +1,8 @@
 const sounds = {
-  wind: new Audio("../sounds/spring-weather.wav"),
-  skiUp: new Audio("../sounds/ski1.wav"),
-  skiDown: new Audio("../sounds/ski2.wav"),
-  collision: new Audio("../sounds/collision.wav"),
-  drink: new Audio("../sounds/drink.wav"),
-  wasted: new Audio("../sounds/wasted.mp3"),
+  wind: new Audio("sounds/spring-weather.wav"),
+  skiUp: new Audio("sounds/ski1.wav"),
+  skiDown: new Audio("sounds/ski2.wav"),
+  collision: new Audio("sounds/collision.wav"),
+  drink: new Audio("sounds/drink.wav"),
+  wasted: new Audio("sounds/wasted.mp3"),
 };
